e2ecbad88f17d8f6379fac676fa58562e1ac48d2
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-12-30 17:27:38 -0500

    images.js made

---
+++ src/images.js
@@ -0,0 +1,33 @@
+import akita from './images/akita.jpg';
+import bernie from './images/bernie.jpg';
+import corgi from './images/corgi.jpg';
+import dachshund from './images/dachshund.jpg';
+import golden from './images/golden.jpg';
+import husky from './images/husky.jpg';
+import iggy from './images/iggy.jpg';
+import norwich from './images/norwich.jpg';
+import pom from './images/pom.jpg';
+import poodle from './images/poodle.jpg';
+import pug from './images/pug.jpg';
+import sheltie from './images/sheltie.jpg';
+import shibe from './images/shibe.jpg';
+import westie from './images/westie.jpg';
+
+const images = [
+  akita,
+  bernie,
+  corgi,
+  dachshund,
+  golden,
+  husky,
+  iggy,
+  norwich,
+  pom,
+  poodle,
+  pug,
+  sheltie,
+  shibe,
+  westie
+];
+
+export default images;
