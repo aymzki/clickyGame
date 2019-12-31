516f4467d140112707d0e1c1b602f51b41516a00
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-12-31 14:19:48 -0500

    app.js and relabeling components

--- src/App.js
+++ src/App.js
@@ -5,36 +5,129 @@ import Footer from "./components/Footer";
 import Banner from "./components/Banner";
 import images from "./images";
 
+
 class App extends Component {
-  //interactive component of clicky
-  
+  state = {
+    score: 0,
+    highScore: 0,
 
-  // removeFriend = id => {
-  //   // Filter this.state.friends for friends with an id not equal to the id being removed
-  //   const friends = this.state.friends.filter(friend => friend.id !== id);
-  //   // Set this.state.friends equal to the new friends array
-  //   this.setState({ friends });
-  // };
+    // stores the class value to assign to navMessage based on a good or bad click
+    navMsgColor: '',
 
-  // // Map over this.state.friends and render a FriendCard component for each friend object
-  // render() {
-  //   return (
-  //     <Wrapper>
-  //       <Title>Friends List</Title>
-  //       {this.state.friends.map(friend => (
-  //         <FriendCard
-  //           removeFriend={this.removeFriend}
-  //           id={friend.id}
-  //           key={friend.id}
-  //           name={friend.name}
-  //           image={friend.image}
-  //           occupation={friend.occupation}
-  //           location={friend.location}
-  //         />
-  //       ))}
-  //     </Wrapper>
-  //   );
+    // contains intro, success, and failure message
+    navMessage: 'Click a puppy to start!',
+
+    // contains an array of image urls
+    allCharacters: this.shuffleArray(),
+
+    // will track  each clicked element.
+    wasClicked: [],
+
+    // shakes the container on an incorrect guess if set to true
+    shake: false
+  };
+
+  // binds the current this context to checkClicked to have access to the current state
+  // when passed down to the Character component
+  clickEvent = this.checkClicked.bind(this);
+
+  // used to shuffle the array of images when the DOM loads, and when an image is clicked
+  shuffleArray() {
+    // creates a copy of the current characters array to modify it by value, and not by reference
+    const newArr = images.slice();
+
+    // will store the shuffled array
+    const shuffleArr = [];
+
+    // each loop through an index gets spliced from newArr, reducing its length
+    // gets a random index based off the current length of newArr
+    // splices the value from newArr, and pushes it to shuffleArr
+    while (newArr.length > 0) {
+      shuffleArr.push(newArr.splice(Math.floor(Math.random() * newArr.length), 1)[0]);
+    }
+
+    return shuffleArr;
   }
 
+  checkClicked(clickedElem) {
+    // creates a copy of the wasClicked array to modify it by value, and not by reference. wasClicked stores all previous clicked images
+    const prevState = this.state.wasClicked.slice();
+
+    // shuffles the images
+    const shuffled = this.shuffleArray();
+
+    // tracks score
+    let score = this.state.score;
+    let highScore = this.state.highScore;
+
+    // if the clicked item is not in wasClicked, then it hasn't been clicked and the score is increased
+    if (!this.state.wasClicked.includes(clickedElem)) {
+      // if score and highScore are the same, then there is a new highScore value
+      if (score === highScore) {
+        score++;
+        highScore++;
+
+        // if they are not equal, then only increase the score value
+      } else {
+        score++;
+      }
+
+      // adds the clicked item to wasClicked to track that it has been clicked
+      prevState.push(clickedElem);
+    }
+
+    // resets the current score if the same element was clicked twice
+    if (this.state.wasClicked.includes(clickedElem)) {
+      let score = 0;
+      return this.setState({
+        score: score,
+        highScore: highScore,
+        navMsgColor: 'incorrect',
+        navMessage: 'Incorrect guess!',
+        allCharacters: shuffled,
+        wasClicked: [],
+        shake: true
+      });
+    }
+
+    // if this runs, then the same element has not been clicked twice and the score is increased
+    this.setState({
+      score: score,
+      highScore: highScore,
+      navMsgColor: 'correct',
+      navMessage: 'You Guessed Correctly!',
+      allCharacters: shuffled,
+      wasClicked: prevState,
+      shake: false
+    });
+
+    // removes the green correct indicator on a successful click after .5s to re-render the class on each success
+    return setTimeout(() => this.setState({ navMsgColor: '' }), 500);
+  }
+
+  // renders score to the navbar.
+  // passes the randomized state.allCharacters array to Container to create a Character component for each image.
+  // passes the this.checkClicked down to container to pass to each Character component to be used for the click event.
+  render() {
+    const state = this.state;
+    return (
+      <div>
+        <Navbar
+          score={state.score}
+          highScore={state.highScore}
+          navMessage={state.navMessage}
+          navMsgColor={state.navMsgColor}
+        />
+        <Banner />
+        <Container
+          shake={state.shake}
+          characters={state.allCharacters}
+          clickEvent={this.clickEvent}
+        />
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/Puppies/index.js
+++ src/components/Puppies/index.js
@@ -1,1 +1,12 @@
-export { default } from './Puppies';
+import React from 'react';
+import './puppies.css';
+
+// the onClick function is defined as a callback so that the clicked elements value
+// can be passed to props.clickEvent to check if the image has been clicked or not
+const Puppies = props => (
+  <div className="card" onClick={e => props.clickEvent(e.target.src)}>
+    <img className="card-img-top card-height" src={props.images} alt="" />
+  </div>
+);
+
+export default Puppies;
