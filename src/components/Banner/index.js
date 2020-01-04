4db64a7eda62d96de7dbad7ad0fb2e845f6a0c29
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2020-01-04 05:55:04 -0500

    edited banner text

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 const Banner = () => (
   <div className="banner text-center d-flex align-items-center justify-content-center">
-    <p className="m-0">Adopt all the puppies, but you can't choose the same one!</p>
+    <p className="m-0">Adopt all the puppies without choosing the same one twice!</p>
   </div>
 );
 export default Banner;
